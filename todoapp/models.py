5576feeb932e6f85b0356641cb8d8cb144a96e31
GeroZayas <gerozayas@gmail.com>
2023-01-21 11:42:23 +0100

    procfile name app config

---
+++ todoapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+        ordering = ['-created']
+
+    title = models.CharField(max_length=350)
+    completed = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
