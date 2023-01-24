64616366e4e31a38ac4f399276e6acc1e0f35e4d
GeroZayas <gerozayas@gmail.com>
2023-01-24 13:35:20 +0100

    update views

--- todoapp/views.py
+++ todoapp/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render, HttpResponse, redirect
 from .forms import TaskForm
 from .models import Task
 
-# from django.views.decorators.csrf import ensure_csrf_cookie
-from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 # Create your views here.
-# @ensure_csrf_cookie
-@csrf_protect
+@ensure_csrf_cookie
 def index(request):
     form = TaskForm()
     if request.method == "POST":
@@ -21,8 +19,7 @@ def index(request):
     return render(request, "index.html", {"task_form": form, "tasks": tasks})
 
 
-# @ensure_csrf_cookie
-@csrf_protect
+@ensure_csrf_cookie
 def update_task(request, pk):
     # pk -> primary key
     task = Task.objects.get(id=pk)
@@ -38,8 +35,7 @@ def update_task(request, pk):
     return render(request, "update_task.html", {"task_edit_form": form})
 
 
-# @ensure_csrf_cookie
-@csrf_protect
+@ensure_csrf_cookie
 def delete_task(request, pk):
     task = Task.objects.get(id=pk)
     task.delete()
