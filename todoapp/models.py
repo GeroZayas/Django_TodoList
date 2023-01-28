2933f721c87448cb4408d54d9049ed02219137c1
GeroZayas <gerozayas@gmail.com>
2023-01-28 22:26:06 +0100

    add image filed model

--- todoapp/models.py
+++ todoapp/models.py
@@ -5,11 +5,12 @@ class Task(models.Model):
     class Meta:
         verbose_name = "Task"
         verbose_name_plural = "Tasks"
-        ordering = ['-created']
+        ordering = ["-created"]
 
     title = models.CharField(max_length=350)
     completed = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="static\images")
 
     def __str__(self):
         return self.title
